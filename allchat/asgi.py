6b806578456fc89a4874576a952bf84fc1b3cb88
Gustavo Aviles <gustavo.avil97@gmail.com>
2024-08-07 14:39:12 -0400

    init

---
+++ allchat/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for allchat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'allchat.settings')
+
+application = get_asgi_application()
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO WORLD</h1>
+      <h2>Im making another change</h2>
+    </div>
+  );
+}
+
+export default App;
